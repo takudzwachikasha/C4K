2f5848e74367b4f003ba1cc006e380bf3b8d71ba
Takudzwa Chikasha <harrisonchikks@gmail.com>
2025-01-29 11:44:25 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add interactivity (e.g., smooth scrolling, animations)
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
